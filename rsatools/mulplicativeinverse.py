7edd549e63e8671d264dd38161e0e91d8ab592ed
SteelShredder <gavingavington@gmail.com>
2018-08-31 10:28:29 -0700

    Update mulplicativeinverse.py

--- rsatools/mulplicativeinverse.py
+++ rsatools/mulplicativeinverse.py
@@ -1,4 +1,8 @@
 from .eea import eea
+# This module is licensed under CC BY-SA 3.0
+# Human readable https://creativecommons.org/licenses/by-sa/3.0/
+# Actual license https://creativecommons.org/licenses/by-sa/3.0/legalcode
+# From https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
 def mulinv(b, n):
     """ performs mulplicative inverse """
     
